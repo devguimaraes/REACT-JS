a977842eb97227bb7886a597cab194dd0d4b7428
Bruno Guimaraes <devgmrs@gmail.com>
2022-08-16 21:21:50 -0300

    :pencil: novos atributos Times

--- src/data/times.jsx
+++ src/data/times.jsx
@@ -1,11 +1,39 @@
 const times = [
-	'Programação',
-	'Front-End',
-	'Data Science',
-	'Devops',
-	'UX e Design',
-	'Mobile',
-	'Inovação e Gestão',
+	{
+		nome: 'Programação',
+		primary: '#57C278',
+		secundary: '#D9F7E9',
+	},
+	{
+		nome: 'Front-End',
+		primary: '#82CFFA',
+		secundary: '#E8F8FF',
+	},
+	{
+		nome: 'Data Science',
+		primary: '#A6D157',
+		secundary: '#F0F8E2',
+	},
+	{
+		nome: 'Devops',
+		primary: '#E06B69',
+		secundary: '#FDE7E8',
+	},
+	{
+		nome: 'UX e Design',
+		primary: '#DB6EBF',
+		secundary: '#FAE9F5',
+	},
+	{
+		nome: 'Mobile',
+		primary: '#FFBA05',
+		secundary: '#FFF5D9',
+	},
+	{
+		nome: 'Inovação e Gestão',
+		primary: '#FF8A29',
+		secundary: '#FFEEDF',
+	},
 ];
 
 export default times;
